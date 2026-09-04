d828f096a1427e35d0993105afcc0a60b18f3a33
poppop0307 <144186712+poppop0307@users.noreply.github.com>
2026-09-04 17:32:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyAlklkIM7C086jhIHpnumxceayb-PIvPVg",
+    authDomain: "pllll-429ed.firebaseapp.com",
+    projectId: "pllll-429ed",
+    storageBucket: "pllll-429ed.firebasestorage.app",
+    messagingSenderId: "299339940502",
+    appId: "1:299339940502:web:c4f514eebc4dfc859a78b7"
+  };
 
 // 파이어베이스 초기화
 firebase.initializeApp(firebaseConfig);
